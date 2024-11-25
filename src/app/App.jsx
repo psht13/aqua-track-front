cd0c3a561b7139c494bf8ec4b98cf6bff578e852
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-11-25 09:23:51 +0200

    fix: missed path

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router";
-import SharedLayout from "../components/SharedLayout/SharedLayout";
 
 import HomePage from "../pages/home-page/HomePage";
 import css from "./App.module.css";
+import SharedLayout from "../components/sharedLayout/SharedLayout";
 
 const App = () => {
   return (
     <div className={css.container}>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={<SharedLayout />}>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
         </Route>
       </Routes>
     </div>
