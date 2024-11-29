d6840249510bb8a899a4854ef72a829c5a5dff02
rovex65 <rovex65@gmail.com>
2024-11-29 18:16:31 +0100

    fix routes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -11,9 +11,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Route>
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </div>
   );
