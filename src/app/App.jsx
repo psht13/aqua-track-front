233c27a74d1ed6d39183ee06858000ec6fdf41ed
Pavlo Yurchenko <52372416+psht13@users.noreply.github.com>
2024-12-02 15:19:08 +0000

    update: fix

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -14,6 +14,7 @@ const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
 const SignInPage = lazy(() => import("../pages/SignInPage/SignInPage"));
 const SignUpPage = lazy(() => import("../pages/SignUpPage/SignUpPage"));
 const TrackerPage = lazy(() => import("../pages/TrackerPage/TrackerPage"));
+const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage.jsx"));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -64,6 +65,7 @@ const App = () => {
               </PrivateRoute>
             }
           />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </div>
