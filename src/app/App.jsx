e597f267591a32dc088dab94b4a4902f68b1f3b5
Olha Nahaichuk <olyasikaa@gmail.com>
2024-12-02 16:55:39 +0200

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,44 +1,71 @@
-import { Route, Routes } from "react-router";
-import HomePage from "../pages/HomePage/HomePage";
+import { Route, Routes } from "react-router-dom";
+import { lazy, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import SharedLayout from "../components/SharedLayout/SharedLayout";
-import SignInPage from "../pages/SignInPage/SignInPage";
-import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import PublicRoute from "../components/PublicRoute";
+import PrivateRoute from "../components/PrivateRoute";
 import css from "./App.module.css";
+import { selectAuthIsRefreshing } from "../redux/selectors";
+import { apiRefreshUser } from "../redux/operations";
+import { Suspense } from "react";
+
+// Лейзі імпорти для сторінок
+const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
+const SignInPage = lazy(() => import("../pages/SignInPage/SignInPage"));
+const SignUpPage = lazy(() => import("../pages/SignUpPage/SignUpPage"));
+const TrackerPage = lazy(() => import("../pages/TrackerPage/TrackerPage"));
 
 const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectAuthIsRefreshing);
+
+  useEffect(() => {
+    dispatch(apiRefreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <div className={css.container}>
-      <Routes>
-        <Route
-          path="/"
-          element={<SharedLayout />}>
+      <Suspense fallback={<div>Loading...</div>}>
+        {" "}
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <HomePage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="signin"
+              element={
+                <PublicRoute>
+                  <SignInPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="signup"
+              element={
+                <PublicRoute>
+                  <SignUpPage />
+                </PublicRoute>
+              }
+            />
+          </Route>
           <Route
-            path="/"
+            path="/tracker"
             element={
-              <PublicRoute>
-                <HomePage />
-              </PublicRoute>
+              <PrivateRoute>
+                <TrackerPage />
+              </PrivateRoute>
             }
           />
-          <Route
-            path="/signin"
-            element={
-              <PublicRoute>
-                <SignInPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <PublicRoute>
-                <SignUpPage />
-              </PublicRoute>
-            }
-          />
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
