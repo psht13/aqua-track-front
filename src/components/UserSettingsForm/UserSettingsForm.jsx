1a2cef955804ac5716db596eaebe8451b06ba38e
Serhii Borysenko <borisenko37@gmail.com>
2024-12-05 09:18:47 +0100

    again

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { BsExclamationLg } from "react-icons/bs";
+import { selectUser } from "../../redux/user/selectors";
 
 import * as Yup from "yup";
 import axios from "axios";
