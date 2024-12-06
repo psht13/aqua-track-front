ffbd757a38fe38de7501f1997d732af9ef6a8283
Patter1989 <125509804+Patter1989@users.noreply.github.com>
2024-12-06 04:23:29 +0100

    styles and svg

--- src/components/WaterItem/WaterItem.jsx
+++ src/components/WaterItem/WaterItem.jsx
@@ -4,6 +4,7 @@ import css from "../WaterItem/WaterItem.module.css";
 import glass from '../../assets/sprite.svg#icon-glass';
 import DeleteWaterModal from "../DeleteWaterModal/DeleteWaterModal.jsx";
 
+
 const WaterItem = ({ item: { id, date, amount } }) => {
   const [isEditModalOpen, setEditModalIsOpen] = useState(false);
   const [isDeleteModalOpen, setDeleteModalIsOpen] = useState(false);
@@ -20,54 +21,62 @@ const WaterItem = ({ item: { id, date, amount } }) => {
   };
 
   return (
-    <>
-      <div className={css.waterContainer}>
-        <svg className={css.icon} width={32} height={32}>
-                  <use href={glass} />
-                </svg>
-        <div className={css.details}>
-          <span className={css.amount}>{amount} ml</span>
-          <span className={css.time}>{formatTime(date)}</span>
-        </div>
-        <div className={css.actions}>
-          <button
-            className={css.actionButton}
-            onClick={() => {
-              openModalEdit(
-                <WaterModal
-                  operationType="edit"
-                  id={id}
-                  amount={amount}
-                  myTime={date}
-                />
-              );
-            }}
-          >
-            <svg
-              width={14}
-              height={14}
-              className={css.btnSvg}
-              //   style={{ stroke: "var(--main)" }}
-            >
-              <use href="src/assets/sprite.svg#icon-upload"></use>
-            </svg>
-          </button>
-          <button
-            className={css.actionButton}
-            onClick={() => {
-              openModalDelete(<DeleteWaterModal id={id} />);
-            }}
-            type="button"
-            aria-label="Delete item"
-          >
-            <svg width={14} height={14} className={css.btnSvg}>
-              <use href="src/assets/sprite.svg#icon-trash" />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </>
-  );
+		<>
+			<div className={css.waterContainer}>
+				<svg
+					className={css.icon}
+					width={32}
+					height={32}
+				>
+					<use href={glass} />
+				</svg>
+				<div className={css.details}>
+					<span className={css.amount}>{amount} ml</span>
+					<span className={css.time}>{formatTime(date)}</span>
+				</div>
+				<div className={css.actions}>
+					<button
+						className={css.actionButton}
+						onClick={() => {
+							openModalEdit(
+								<WaterModal
+									operationType='edit'
+									id={id}
+									amount={amount}
+									myTime={date}
+								/>
+							);
+						}}
+					>
+						<svg
+							width={14}
+							height={14}
+							className={css.btnSvg}
+							//   style={{ stroke: "var(--main)" }}
+						>
+							<use href='src/assets/sprite.svg#icon-edit'></use>
+						</svg>
+					</button>
+					<button
+						className={css.actionButton}
+						onClick={() => {
+							openModalDelete(<DeleteWaterModal id={id} />);
+						}}
+						type='button'
+						aria-label='Delete item'
+					>
+						<svg
+							width={14}
+							height={14}
+							className={css.btnSvg}
+						>
+							<use href='src/assets/sprite.svg#icon-trash' />
+						</svg>
+					</button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default WaterItem;
